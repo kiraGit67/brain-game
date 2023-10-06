85daa26dde62bd6ba62dee6aa74e8912db24ddc9
kiraGit67 <kirsten@sais.de>
2023-10-06 19:53:56 +0200

    adds function to generate random colors

--- script.js
+++ script.js
@@ -1,5 +1,17 @@
 $(document).ready(function () {
+  //Function to generate random colors
+  function generateRandomColor() {
+    var randomColor = "#" + Math.floor(Math.random() * 16777215).toString(16);
+    return randomColor;
+  }
+
+  let colorLeft, colorCenter, colorRight;
+
   $(".square").click(function () {
     alert("Viereck wurde geklickt.");
+
+    colorLeft = generateRandomColor();
+    colorCenter = generateRandomColor();
+    colorRight = generateRandomColor();
   });
 });
